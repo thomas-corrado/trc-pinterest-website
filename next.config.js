51f8d156d5de4f39ae98964aa4cc4fd22375c9c7
Thomas Corrado <thomas.r.corrado.25@dartmouth.edu>
2026-07-20 13:01:42 -0500

    fix api call thing

--- next.config.js
+++ next.config.js
@@ -3,13 +3,17 @@ const nextConfig = {
   images: {
     // This tells Next.js to automatically serve AVIF or WebP to browsers that support them
     formats: ["image/avif", "image/webp"],
+
+    // MOVED HERE: Now Next.js knows these remote patterns belong to the image optimizer
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "*.public.blob.vercel-storage.com",
-    },
-  ],
 };
 
 module.exports = nextConfig;
